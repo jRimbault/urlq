919d3253322db0c7909f48367d2297596d88df71
jRimbault <jacques.rimbault@gmail.com>
2023-04-24 14:53:53 +0200

    put more test cases into files

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,26 +3,16 @@ use std::path::PathBuf;
 use predicates::prelude::*;
 
 #[rstest::rstest]
-#[case(&["https://curl.se", "set", "host=example.com"], "https://example.com/\n")]
-#[case(&["https://curl.se/we/../are.html", "set", "port=8080"], "https://curl.se:8080/are.html\n")]
-#[case(&["https://curl.se/we/are.html", "get", "path"], "/we/are.html\n")]
-#[case(&["https://curl.se/we/are.html", "get", "port"], "443\n")]
-#[case(&["https://example.com/hello.html", "get", "scheme", "port", "path"], "https 443 /hello.html\n")]
-fn test(#[case] args: &[&str], #[case] stdout: &str) {
-    assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME"))
-        .unwrap()
-        .args(args)
-        .assert()
-        .success()
-        .stdout(predicate::eq(stdout));
-}
-
-#[rstest::rstest]
 #[case(&["get", "host"], "cases/01")]
 #[case(&["https://fake.host/hello#frag", "--json", "set", "user=::moo::"], "cases/02")]
 #[case(&["https://fake.host/hello#frag", "set", "user=::moo::"], "cases/03")]
-fn file(#[case] args: &[&str], #[case] path: &str) {
-    let path = PathBuf::from("tests").join(path);
+#[case(&["https://curl.se", "set", "host=example.com"], "cases/04")]
+#[case(&["https://curl.se/we/../are.html", "set", "port=8080"], "cases/05")]
+#[case(&["https://curl.se/we/are.html", "get", "path"], "cases/06")]
+#[case(&["https://curl.se/we/are.html", "get", "port"], "cases/07")]
+#[case(&["https://example.com/hello.html", "get", "scheme", "port", "path"], "cases/08")]
+fn test(#[case] args: &[&str], #[case] path: &str) {
+    let path: PathBuf = ["tests"].into_iter().chain(path.split('/')).collect();
     let stdin = std::fs::read_to_string(path.join("stdin.txt")).unwrap_or_default();
     let stdout = std::fs::read_to_string(path.join("stdout.txt")).unwrap();
     assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME"))

81307792c25754ccac98d58071c029c4e69a5537
jRimbault <jacques.rimbault@gmail.com>
2023-04-24 10:49:18 +0200

    use rstest

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,7 +1,15 @@
+use std::path::PathBuf;
+
 use predicates::prelude::*;
 
-fn test_case(args: &[&str], stdout: &str) {
-    assert_cmd::Command::cargo_bin("urlq")
+#[rstest::rstest]
+#[case(&["https://curl.se", "set", "host=example.com"], "https://example.com/\n")]
+#[case(&["https://curl.se/we/../are.html", "set", "port=8080"], "https://curl.se:8080/are.html\n")]
+#[case(&["https://curl.se/we/are.html", "get", "path"], "/we/are.html\n")]
+#[case(&["https://curl.se/we/are.html", "get", "port"], "443\n")]
+#[case(&["https://example.com/hello.html", "get", "scheme", "port", "path"], "https 443 /hello.html\n")]
+fn test(#[case] args: &[&str], #[case] stdout: &str) {
+    assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME"))
         .unwrap()
         .args(args)
         .assert()
@@ -9,75 +17,19 @@ fn test_case(args: &[&str], stdout: &str) {
         .stdout(predicate::eq(stdout));
 }
 
-#[test]
-fn example1() {
-    test_case(
-        &["https://curl.se", "set", "host=example.com"],
-        "https://example.com/\n",
-    );
-}
-
-#[test]
-fn example2() {
-    test_case(
-        &["https://curl.se/we/../are.html", "set", "port=8080"],
-        "https://curl.se:8080/are.html\n",
-    );
-}
-
-#[test]
-fn example3() {
-    test_case(
-        &["https://curl.se/we/are.html", "get", "path"],
-        "/we/are.html\n",
-    );
-}
-
-#[test]
-fn example4() {
-    test_case(&["https://curl.se/we/are.html", "get", "port"], "443\n");
-}
-
-#[test]
-fn example5() {
-    test_case(
-        &[
-            "https://example.com/hello.html",
-            "get",
-            "scheme",
-            "port",
-            "path",
-        ],
-        "https 443 /hello.html\n",
-    );
-}
-
-#[test]
-fn example6() {
-    assert_cmd::Command::cargo_bin("urlq")
+#[rstest::rstest]
+#[case(&["get", "host"], "cases/01")]
+#[case(&["https://fake.host/hello#frag", "--json", "set", "user=::moo::"], "cases/02")]
+#[case(&["https://fake.host/hello#frag", "set", "user=::moo::"], "cases/03")]
+fn file(#[case] args: &[&str], #[case] path: &str) {
+    let path = PathBuf::from(format!("tests/{path}"));
+    let stdin = std::fs::read_to_string(path.join("stdin.txt")).unwrap_or_default();
+    let stdout = std::fs::read_to_string(path.join("stdout.txt")).unwrap();
+    assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME"))
         .unwrap()
-        .args(["get", "host"])
-        .write_stdin(std::fs::read("tests/url-list.txt").unwrap())
+        .args(args)
+        .write_stdin(stdin)
         .assert()
         .success()
-        .stdout(predicate::eq(
-            "curl.se
-fake.host
-example.net
-",
-        ));
-}
-
-#[test]
-fn example7() {
-    test_case(
-        &[
-            "https://fake.host/hello#frag",
-            "--json",
-            "set",
-            "user=::moo::",
-        ],
-        r#"{"url":"https://%3A%3Amoo%3A%3A@fake.host/hello#frag","fragment":"frag","host":"fake.host","path":"/hello","port":"443","scheme":"https","user":"%3A%3Amoo%3A%3A"}
-"#,
-    )
+        .stdout(predicate::eq(stdout));
 }
